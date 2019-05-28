b76f6043636377393a75e22bce98f13b85a76aff
ShivaniMoeay <morayshivani@gmail.com>
2019-05-28 22:45:02 +0200

    app.js with basic routes and registration page code

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
 //installed init, express, ejs, body-parser, mongoose, passport, cookieparser, passportlocal, passportlocalmongoose, session, flash
 
 var express = require("express"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    bodyParser = require("body-parser"),
-    cookieParser = require('cookie-parser'),
-    User = require("./models/user"),
-    LocalStrategy = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose"),
-    UserDetail = require("./models/userdetail"),
-    CustUserDetail = require("./models/recuserdetails"), //Customer user details
-    upload = require('./helpers/multer'),
-    session = require('express-session'),
-    flash = require('connect-flash')
+    //mongoose = require("mongoose"),
+    //passport = require("passport"),
+    bodyParser = require("body-parser")
+    var app = express();
+    //cookieParser = require('cookie-parser'),
+    //User = require("./models/user"),
+    //LocalStrategy = require("passport-local"),
+    //passportLocalMongoose = require("passport-local-mongoose"),
+    //UserDetail = require("./models/userdetail"),
+    //CustUserDetail = require("./models/recuserdetails"), //Customer user details
+    //upload = require('./helpers/multer'),
+    //session = require('express-session'),
+    //flash = require('connect-flash')
 
-mongoose.connect("mongodb://localhost:27017/gallery");
-var db = mongoose.connection;
+//mongoose.connect("mongodb://localhost:27017/gallery");
+//var db = mongoose.connection;
 
 //Check connection to mongo
-db.once('open', function () {
-    console.log('Connected to mongodb');
-});
+//db.once('open', function () {
+  //  console.log('Connected to mongodb');
+//});
 
 //Initialization of express and body-parser
-var app = express();
-app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(__dirname + '/'));
 
-//Passport initialization
-app.use(passport.initialize());
-app.use(passport.session());
 
-//Storing current user details
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-    res.locals.currentUser = req.user;
-    res.locals.messages = require('express-messages')(req, res);
-    next();
-});
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 //Server port
 app.listen(3000, function(){
@@ -51,44 +37,36 @@ app.listen(3000, function(){
 });
 
 //Landing page
-app.get("/", function (req, res) {
-    res.render('landing');
+app.get("/register", function (req, res) {
+    res.render('register.ejs');
 });
+
+//Homepage routes
+var homepage = require('/homepage');
+res.render('/homepage', homepage)
 
 //Logout
-app.get("/logout", function (req, res) {
-    req.logout();
-    req.flash('success', 'You have logged out successfully!')
-    res.redirect("/");
-});
+//app.get("/logout", function (req, res) {
+ //   req.logout();
+  //  req.flash('success', 'You have logged out successfully!')
+   // res.redirect("/");
+//});
 
 //Show all paintings and filter painting types on homepage
-app.get("/homepage", isLoggedIn, function (req, res) {
-    var type = req.params.artist;
-    UserDetail.find({}, function (err, artists) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('homepage', {
-                artists: artists
-            })
-        }
-    })
-})
+//app.get("/homepage", isLoggedIn, function (req, res) {
+ //   var type = req.params.artist;
+ //   UserDetail.find({}, function (err, artists) {
+ //       if (err) {
+  //          console.log(err);
+  //      } else {
+  //          res.render('homepage', {
+  //              artists: artists
+   //         })
+   //     }
+   // })
+//})
 
-app.get("/homepage/:artist", isLoggedIn, function (req, res) {
-    var type = req.params.artist;
-    UserDetail.find({ 'details.artist': type }, function (err, artists) {
-        if (err) {
-            req.flash('danger', 'Oops! Something went wrong, please try again. ')
-            console.log(err);
-        } else {
-            res.render('homepage', {
-                artists: artists,
-            })
-        }
-    })
-})
+
 
 
 
